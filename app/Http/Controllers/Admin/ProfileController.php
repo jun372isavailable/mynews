24ca7e8459983ba590ce8d82fa1ff410e46523bd
nagayama <long.mountain@outlook.com>
2021-03-22 05:51:06 +0000

    heroku

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,20 +15,8 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create(Request $request)
+    public function create()
     {
-        $this->validate($request, Profiles::$rules);
-        
-        $profile = new Profiles;
-        $form = $request->all();
-        
-    
-       
-        unset($form['_token']);
-        unset($form['image']);
-        $profile->fill($form);
-        $profile->save();
-    
         return redirect('admin/profile/create');
     }
     public function edit()
@@ -38,17 +26,6 @@ class ProfileController extends Controller
     
     public function update()
     {
-        return redirect('admin/profile/create');
-    }
-    
-    public function index(Request $request)
-    {
-        $cond_title = $request->cond_title;
-        if ($cond_title !='') {
-            $posts = Profiles::where('title', $cond_title)->get();
-        } else {
-            $posts = Profiles::all();
-        }
-        return view('admin.profile.index', ['posts' => $posts,'cond_title' => $cond_title]);
+        return redirect('admin/profile/edit');
     }
 }
